88e5f81b27707ec99d670f43682e0a6b11224d3e
liuyazong <liuyazong@gmail.com>
2018-03-28 13:16:18 +0800

    add ssl support

--- grpc-client-demo/src/main/java/yz/ClientDemoApp.java
+++ grpc-client-demo/src/main/java/yz/ClientDemoApp.java
@@ -2,12 +2,10 @@ package yz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ClientDemoApp {
-    public static void main(String[] args) throws InterruptedException {
-        ConfigurableApplicationContext context = SpringApplication.run(ClientDemoApp.class, args);
-        Thread.currentThread().join();
+    public static void main(String[] args) {
+        SpringApplication.run(ClientDemoApp.class, args);
     }
 }
